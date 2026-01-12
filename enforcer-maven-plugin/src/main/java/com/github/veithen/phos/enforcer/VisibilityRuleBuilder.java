11ca88f5b340d89044b16d2ecb40562c5de527be
Andreas Veithen <andreas.veithen@gmail.com>
2026-01-12 09:34:12 +0000

    Update license headers

--- enforcer-maven-plugin/src/main/java/com/github/veithen/phos/enforcer/VisibilityRuleBuilder.java
+++ enforcer-maven-plugin/src/main/java/com/github/veithen/phos/enforcer/VisibilityRuleBuilder.java
@@ -2,7 +2,7 @@
  * #%L
  * Phos
  * %%
- * Copyright (C) 2016 - 2023 Andreas Veithen
+ * Copyright (C) 2016 - 2026 Andreas Veithen-Knowles
  * %%
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
